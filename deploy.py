034891cbf8f32e8b9ee70f6c21bbdbe7b8892f25
Brian Liao <phyred23@gmail.com>
2020-02-29 13:33:22 -0800

    update repo

--- deploy.py
+++ deploy.py
@@ -8,9 +8,8 @@ subprocess.run('git push', shell=True)
 subprocess.run('mdbook build', shell=True)
 subprocess.run('rm -rf release/*', shell=True) # this won't delete the .git directory
 subprocess.run('cp -rp book/* release/', shell=True)
-subprocess.run('cd release', shell=True)
-subprocess.run('git add -A', shell=True)
-subprocess.run('git commit -m \'update book\'', shell=True)
-subprocess.run('git push origin gh-pages', shell=True)
-subprocess.run('cd -', shell=True)
-# subprocess.run('git checkout master', shell=True)
+
+subprocess.run('cd release; git add -A', shell=True)
+subprocess.run('cd release; git commit -m \'update book\'', shell=True)
+subprocess.run('cd release; git push origin gh-pages', shell=True)
+
