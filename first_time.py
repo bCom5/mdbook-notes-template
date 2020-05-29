96885cb0d85d1819453ceb87e2596292708ff51a
Brian Liao <phyred23@gmail.com>
2020-05-29 16:31:02 -0700

    update repo

--- first_time.py
+++ first_time.py
@@ -23,8 +23,8 @@ line_fmt = 'open -a \"/Applications/Google Chrome.app\" {}'
 '''
 quick_start += f'''
 subprocess.run(line_fmt.format(f'http://localhost:{port}/'), shell=True)
-subprocess.run(f'subl .', shell=True)
-subprocess.run(f'subl src/SUMMARY.md', shell=True)
+subprocess.run(f'mate .', shell=True)
+subprocess.run(f'mate src/SUMMARY.md', shell=True)
 subprocess.run(f'mdbook serve -p {port}', shell=True)
 '''
 
