2e0a5c79e532bcaa71d8d7e03a89612fd3b25659
Brian Liao <phyred23@gmail.com>
2020-06-10 09:53:21 -0700

    update repo

--- quick_start.py
+++ quick_start.py
@@ -3,6 +3,6 @@ import subprocess
 port = '3024'
 line_fmt = 'open -a \"/Applications/Google Chrome.app\" {}'
 subprocess.run(line_fmt.format(f'http://localhost:{port}/'), shell=True)
-subprocess.run(f'mate .', shell=True)
-subprocess.run(f'mate src/SUMMARY.md', shell=True)
+subprocess.run(f'subl .', shell=True)
+subprocess.run(f'subl src/SUMMARY.md', shell=True)
 subprocess.run(f'mdbook serve -p {port}', shell=True)
